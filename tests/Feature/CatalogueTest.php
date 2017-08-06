f223bff142919c8b3fbd3189f28073393e52b0cc
Ben Clayton <Ben@benclayton.me>
2017-08-06 14:12:40 +0100

    Add testing, Travis and Code Climate integrations

---
+++ tests/Feature/CatalogueTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RuffleLabs\ProductCore\Tests\Feature;
+
+use RuffleLabs\ProductCore\Tests\ProductCoreTestCase;
+
+class CatalogueTest extends ProductCoreTestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/ProductCoreTestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RuffleLabs\ProductCore\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class ProductCoreTestCase extends PHPUnit_Framework_TestCase
+{
+
+}
